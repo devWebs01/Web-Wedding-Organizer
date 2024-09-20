9d6f7ee89e28f1b77e5dbc6d5873b778ab4b17fa
Adi Putra <m.a.syaputra.94@gmail.com>
2024-09-21 01:20:26 +0700

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,17 +8,16 @@ use function Laravel\Folio\name;
 name('welcome');
 
 state([
-    'products' => fn() => Product::inRandomOrder()->limit(6)->get(),
+    'products' => fn() => Product::latest()->limit(3)->get(),
 ]);
 
 ?>
 
 <x-guest-layout>
     <x-slot name="title">Selamat Datang</x-slot>
-
     <style>
         .hover {
-            --c: #ff8e56;
+            --c: #9c9259;
             /* the color */
 
             color: #0000;
@@ -28,75 +27,100 @@ state([
             -webkit-background-clip: text, padding-box;
             background-clip: text, padding-box;
             transition: 0.5s;
-            font-weight: bolder;
+            border-radius: 10px;
         }
 
         .hover:hover {
             --_p: 100%
         }
     </style>
-
     @volt
         <div>
-            <div class="container main-banner">
-                <div class="owl-carousel owl-banner">
-                    <div class="item item-1 rounded rounded-5"
-                        style="background-image: url('/guest/apola_image/banner1.jpg'); width: 100%; height: 900px; object-fit: cover;">
-                        <div class="header-text">
-                            <h2 id="font-custom" class="text-white font-stroke">
-                                Tren fashion terbaru, hanya di jarak selangkah.
+
+            <div class="container pt-5">
+                <div class="row align-items-center my-lg-9">
+                    <div class="col-lg-6">
+                        <div class="banner-content ">
+                            <h6>Momen sekali seumur hidup</h6>
+                            <h2 id="font-custom" class="display-4 fw-semibold my-2 my-lg-3">
+                                Mulailah perjalanan menuju pernikahan yang sempurna
                             </h2>
                         </div>
                     </div>
-                    <div class="item item-2 rounded rounded-5"
-                        style="background-image: url('/guest/apola_image/banner2.jpg'); width: 100%; height: 900px; object-fit: cover;">
-                        <div class="header-text">
-                            <h2 id="font-custom" class="text-white font-stroke">Percayakan gaya Anda pada kami.
-                            </h2>
-                        </div>
-                    </div>
-                    <div class="item item-3 rounded rounded-5"
-                        style="background-image: url('/guest/apola_image/banner3.jpg'); width: 100%; height: 900px; object-fit: cover;">
-                        <div class="header-text">
-                            <h2 id="font-custom" class="text-dark font-stroke">Bergaya sesuai kepribadian Anda dengan
-                                koleksi eksklusif kami.
-                            </h2>
-                        </div>
+                    <div class="col-lg-6">
+                        <p class="fs-5">Pencarian mudah untuk vendor dengan portofolio, harga, dan ulasan dikumpulkan di
+                            satu tempat.</p>
+                        <a href="{{ route('catalog-products') }}"
+                            class="btn btn-outline-dark text-uppercase mt-4 mt-lg-5">Lihat Produk</a>
                     </div>
                 </div>
             </div>
 
-            <div class="properties section">
+            <div class="container my-5 ratio ratio-16x9 border rounded">
+                <iframe width="560" height="315"
+                    src="https://www.youtube.com/embed/079v_9FSf8M?si=8kS51H73iAnFDeHr&amp;start=8"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+
+            <section class="py-5">
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-4 offset-lg-4">
-                            <div class="section-heading text-center">
-                                <h6>| Koleksi Kami</h6>
-                                <h2 id="font-custom" class="fw-bold">Lihat apa yang bisa kamu temukan disini</h2>
-                            </div>
+                        <span class="text-muted">Cerita Kami</span>
+                        <h2 class="display-5 fw-bold">Tentang Kita</h2>
+                        <div class="col-md-5">
+                            <p class="lead">
+                                Kami adalah tim wedding organizer yang berkomitmen untuk membuat hari istimewa Anda menjadi
+                                tak terlupakan. Dengan pengalaman bertahun-tahun, kami memahami setiap detail yang
+                                diperlukan untuk pernikahan yang sempurna.
+                            </p>
+                        </div>
+                        <div class="col-md-6 offset-md-1">
+
+                            <p class="lead mb-0">
+                                Kami percaya bahwa setiap pasangan memiliki cerita unik. Tugas kami adalah
+                                mewujudkan visi Anda menjadi kenyataan, sambil mengurangi stres dan memberikan pengalaman
+                                yang menyenangkan.
+                            </p>
                         </div>
                     </div>
+                </div>
+            </section>
+
+
+            <div class="properties section mt-5">
+                <div class="container">
                     <div class="row">
                         @foreach ($products as $product)
                             <div class="col-lg-4 col-md-6">
-                                <div class="item">
-                                    <a href="{{ route('product-detail', ['product' => $product->id]) }}"><img
-                                            src="{{ Storage::url($product->image) }}" alt="{{ $product->title }}"
-                                            class="object-fit-cover" style="width: 100%; height: 300px;"></a>
-                                    <span class="category">
-                                        {{ Str::limit($product->category->name, 13, '...') }}
-                                    </span>
-                                    <h6>
-                                        {{ Str::limit($product->vendor, 13, '...') }}
-                                    </h6>
-                                    <h4>
+                                <div class="card item text-center border-0 p-4" style="height: 610px">
+
+                                    <div class="card-header border-0 m-0 p-0">
                                         <a href="{{ route('product-detail', ['product' => $product->id]) }}">
-                                            {{ Str::limit($product->title, 50, '...') }}
+                                            <img src="{{ Storage::url($product->image) }}" alt="{{ $product->title }}"
+                                                class="object-fit-cover" style="width: 100%; height: 300px;">
                                         </a>
-                                    </h4>
-                                    <div class="main-button">
-                                        <a href="{{ route('product-detail', ['product' => $product->id]) }}">Beli
-                                            Sekarang</a>
+                                    </div>
+                                    <div class="card-body">
+                                        <span class="category">
+                                            {{ Str::limit($product->category->name, 25, '...') }}
+                                        </span>
+
+                                        <p class="mt-3 mb-0 text-custom fw-bolder">
+                                            {{ Str::limit($product->vendor, 30, '...') }}
+                                        </p>
+
+                                        <h4 class="my-0">
+                                            <a href="{{ route('product-detail', ['product' => $product->id]) }}">
+                                                {{ Str::limit($product->title, 30, '...') }}
+                                            </a>
+                                        </h4>
+                                    </div>
+
+                                    <div class="d-grid card-footer bg-none border-0">
+                                        <a class="btn btn-dark rounded"
+                                            href="{{ route('product-detail', ['product' => $product->id]) }}">Lihat</a>
                                     </div>
                                 </div>
                             </div>
@@ -105,87 +129,25 @@ state([
                 </div>
             </div>
 
-            <section class="py-5">
-                <div class="container">
-                    <!--- Heading -->
-                    <div class="row text-center mb-0">
-                        <div class="col-12 col-lg-10 col-xl-8 mx-auto text-center section-heading">
-                            <h6>| Bersama APOLA</h6>
-                            <h2 class="fw-bold" id="font-custom">Dapatkan produk favorit kamu
-                                <span class="hover">diantar ke depan pintumu</span>
-                            </h2>
-                        </div>
-                    </div><!--- Steps Wrap -->
-                    <div class="row justify-content-center">
-                        <div class="col-lg-9">
-                            <div class="row">
-                                <!-- Step -->
-                                <div class="col-lg-4">
-                                    <div class="text-center position-relative">
-                                        <!-- Step Icon -->
-                                        <div class="step-icon mx-auto border border-2 border rounded-circle d-flex align-items-center justify-content-center"
-                                            style="width:150px;height:150px">
-                                            <img src="/guest/apola_image/LOGO-JNT.png" class="m-3">
-
-                                        </div>
-                                    </div>
-                                </div><!-- Step -->
-                                <div class="col-lg-4">
-                                    <div class="text-center position-relative">
-                                        <!-- Step Icon -->
-                                        <div class="step-icon mx-auto border border-2 border rounded-circle d-flex align-items-center justify-content-center"
-                                            style="width: 150px;height: 150px;">
-                                            <img src="/guest/apola_image/LOGO-TIKI.png" class="m-3">
-                                        </div>
-
-                                    </div>
-                                </div><!-- Step -->
-                                <div class="col-lg-4">
-                                    <div class="text-center position-relative">
-                                        <!--- Step Icon -->
-                                        <div class="step-icon mx-auto border border-2 border rounded-circle d-flex align-items-center justify-content-center"
-                                            style="width: 150px;height: 150px;">
-                                            <img src="/guest/apola_image/LOGO-POS-IND.png" class="m-3">
-                                        </div>
-
-                                    </div>
+            <div class="container">
+                <section class="py-5 my-md-5 border rounded-5" style="background-color: #9c9259;">
+                    <div class="container">
+                        <div class="row justify-content-center text-center text-white py-4">
+                            <div class="col-lg-8">
+                                <span>Daftar Sekarang</span>
+                                <h2 id="font-custom" class="display-5 fw-bold my-2">Mulai Hari Ini Juga!</h2>
+                                <p class="lead text-white">Ceritakan sedikit tentang Anda dan rencana pernikahan Anda agar
+                                    kami dapat memberikan rekomendasi vendor dan konten yang lebih baik.</p>
+                                <div class=" mt-5 d-grid col-3 mx-auto">
+                                    <a class="btn btn-dark text-uppercase " href="{{ route('login') }}"
+                                        type="submit">Daftar</a>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
-
-
-            <div class="video section" id="parallax" style="background-image: url('/guest/apola_image/thumbnail.jpg');">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-4 offset-lg-4">
-                            <div class="section-heading text-center">
-                                <div class="glitch-wrapper">
-                                    <button class="btn btn-dark btn-sm rounded">| APOLA.CO.ID</button>
-                                    <h2 class="glitch" data-glitch="Beli Sekarang!!!"></h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                </section>
             </div>
 
-            <div class="video-content">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-10 offset-lg-1">
-                            <div class="video-frame ratio ratio-16x9">
-                                <video class="rounded-5" muted loop autoplay>
-                                    <source src="{{ asset('/guest/apola_image/videos.mp4') }}" type="video/mp4">
-                                </video>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     @endvolt
 </x-guest-layout>
